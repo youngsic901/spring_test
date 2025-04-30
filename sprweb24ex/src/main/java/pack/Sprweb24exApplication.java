677ad4c2ce6da703d671989b9bf7a97bbb5eb663
youngsic901 <youngsic901@gmail.com>
2025-04-30 10:18:24 +0900

    2025.04.29 JPA ajax

---
+++ sprweb24ex/src/main/java/pack/Sprweb24exApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb24exApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprweb24exApplication.class, args);
+    }
+
+}
